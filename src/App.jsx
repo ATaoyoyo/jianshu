95b7c5b07cdc7982050d429b88aa56a33a01cf09
ATaoyoyo <1029187998@qq.com>
2020-06-26 16:43:58 +0800

    Header组件编写

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Header from './common/header'
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello
+        <Header />
       </div>
     )
   }
---
+++ src/common/header/style.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+import logoPic from '../../statics/logo.png'
+
+export const HeaderWrapper = styled.div`
+  height: 58px;
+  border-bottom: 1px solid #f0f0f0;
+`
+
+export const Logo = styled.a.attrs({ href: '/' })`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100px;
+  height: 58px;
+  background: url(${logoPic});
+  background-size: contain;
+`
+
+export const Nav = styled.div`
+  margin: 0 auto;
+  width: 960px;
+  height: 58px;
+`
+
+export const NavItem = styled.div`
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+    font-size: 15px;
+    color: #969696;
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+  line-height: 58px;
+  padding: 0 15px;
+  font-size: 17px;
+`
+export const NavSearch = styled.input.attrs({
+  placeholder: '搜索',
+})`
+  width: 240px;
+  height: 38px;
+  line-height: 58px;
+  margin-left: 15px;
+  margin-top: 10px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  border-radius: 19px;
+  border: 1px solid #eee;
+  background: #eee;
+  &::placeholder {
+    color: #999;
+  }
+`
+export const Addtion = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 58px;
+`
+
+export const Button = styled.button`
+  font-size: 15px;
+  height: 38px;
+  margin-top: 10px;
+  margin-right: 20px;
+  padding: 6px 12px;
+  border-radius: 19px;
+  border: 1px solid #ea6f5a;
+  &.reg {
+    width: 80px;
+    color: #ea6f5a;
+    background: #fff;
+  }
+  &.writting {
+    width: 100px;
+    color: #fff;
+    background: #ea6f5a;
+  }
+`

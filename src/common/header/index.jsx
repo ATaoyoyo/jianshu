dc8d04e672365daf818519cfcf1a7a25f113805e
ATaoyoyo <1029187998@qq.com>
2020-06-26 19:02:37 +0800

    使用redux,react-redux进行数据管理

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import {
   HeaderWrapper,
@@ -11,66 +12,63 @@ import {
   SearchWrapper,
 } from './style'
 
-class Header extends Component {
-  constructor() {
-    super()
-    this.state = {
-      fouced: false,
-    }
-    this.handleInputFocuce = this.handleInputFocuce.bind(this)
-    this.handleInputBlur = this.handleInputBlur.bind(this)
-  }
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载App</NavItem>
-          <NavItem className="right">登录</NavItem>
-          <NavItem className="right">Aa</NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              in={this.state.fouced}
-              timeout={200}
-              classNames="slide"
-            >
-              <NavSearch
-                className={this.state.fouced ? 'fouced' : ''}
-                onFocus={this.handleInputFocuce}
-                onBlur={this.handleInputBlur}
-              />
-            </CSSTransition>
-            <span
-              className={
-                !this.state.fouced
-                  ? 'iconfont iconfangdajing'
-                  : 'iconfont iconfangdajing fouced'
-              }
-            ></span>
-          </SearchWrapper>
-          <Addtion>
-            <Button className="reg">注册</Button>
-            <Button className="writting">
-              <span className="iconfont iconyumaobi"></span>写文章
-            </Button>
-          </Addtion>
-        </Nav>
-      </HeaderWrapper>
-    )
-  }
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载App</NavItem>
+        <NavItem className="right">登录</NavItem>
+        <NavItem className="right">Aa</NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            in={props.fouce}
+            timeout={200}
+            classNames="slide"
+          >
+            <NavSearch
+              className={props.fouce ? 'fouced' : ''}
+              onFocus={props.handleInputFocuce}
+              onBlur={props.handleInputBlur}
+            />
+          </CSSTransition>
+          <span
+            className={
+              !props.fouce
+                ? 'iconfont iconfangdajing'
+                : 'iconfont iconfangdajing fouced'
+            }
+          ></span>
+        </SearchWrapper>
+        <Addtion>
+          <Button className="reg">注册</Button>
+          <Button className="writting">
+            <span className="iconfont iconyumaobi"></span>写文章
+          </Button>
+        </Addtion>
+      </Nav>
+    </HeaderWrapper>
+  )
+}
 
-  handleInputFocuce() {
-    this.setState({
-      fouced: true,
-    })
-  }
-
-  handleInputBlur() {
-    this.setState({
-      fouced: false,
-    })
+const mapStateToProps = (state) => {
+  return {
+    fouce: state.fouce
   }
 }
 
-export default Header
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocuce() {
+      const action = { type: 'input_fouce' }
+      dispatch(action)
+    },
+    handleInputBlur() {
+      const action = { type: 'input_blur' }
+      dispatch(action)
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
